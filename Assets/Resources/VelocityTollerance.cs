9e84bd7247deca629dbe7bd0128fef0aa0f2401b
Simone <simone.magagna@studenti.unipd.it>
2018-11-08 17:48:29 +0100

    Creato il profilo paziente, le tolleranze e le parti del corpo editabili

---
+++ Assets/Resources/VelocityTollerance.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Velocity", menuName = "Tollerances/Velocity", order = 1)]
+public class VelocityTollerance : ScriptableObject {
+	public string Level;
+	public float Velocity;
+}
