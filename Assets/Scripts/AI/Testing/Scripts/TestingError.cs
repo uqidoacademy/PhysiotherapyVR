6bb90da36119c208dcf42e5929878d6ddaa84b90
Gabriel <ciulei.gabriel@gmail.com>
2018-11-09 16:59:50 +0100

    Testing simulation for movemen traking

---
+++ Assets/Scripts/AI/Testing/Scripts/TestingError.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingError : MonoBehaviour {
+
+    public GameObject Object1;
+    public GameObject Object2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("Error");
+	}
+}
