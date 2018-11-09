c1ec3cc8a03c492014d5aebfadd46eef87b96408
Pietro <pietro.varone@yahoo.it>
2018-11-09 12:47:40 +0100

    added TrackerRenamer script

---
+++ Assets/Scripts/TrackerRenamer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerRenamer : MonoBehaviour {
+
+    List<string> nomi = new List<string>();
+
+    private void Start()
+    {
+        nomi[0] = "Spalla";
+        nomi[1] = "Gomito";
+        nomi[2] = "Mano";
+    }
+
+    //StaticTestList.ArtsList
+
+    public void SetInteraction(bool value)
+    {
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+    
+      
+        for (int i = 0; i < nomi.Count; i++)
+        {
+            other.gameObject.name = nomi[i];
+        }
+
+    }
+
+}
