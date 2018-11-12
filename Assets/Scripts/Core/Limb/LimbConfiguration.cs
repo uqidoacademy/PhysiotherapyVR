29292d87a52cc8be88eb21b3b5f6986cd9aa68bd
Antonio <antonio.terpin@gmail.com>
2018-11-12 14:46:42 +0100

    Documented sensors and limb configuration

--- Assets/Scripts/Core/Limb/LimbConfiguration.cs
+++ Assets/Scripts/Core/Limb/LimbConfiguration.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 namespace Limb
 {
+    /// <summary>
+    /// Configuration of a limb, so describes what the sensors stand for in the real world (e.g. shoulder, elbow, hand, ..)
+    /// </summary>
+    /// <extension>
+    /// To add a limb, extend this class, take as example the ArmConfiguration class
+    /// </extension>
+    /// <author>Antonio Terpin</author>
     public abstract class LimbConfiguration {
         public Dictionary<string, Sensor> sensors = new Dictionary<string, Sensor>();
 
+        /// <summary>
+        /// Take a snapshot of the limb
+        /// </summary>
+        /// <returns>The snapshot of the limb configuration (transforms of the sensors)</returns>
         public abstract LimbData ExtractLimbData();
 
         protected Dictionary<string, Transform> GetTransformOutOfSensors()
@@ -20,6 +31,10 @@ namespace Limb
         }
     }
 
+    /// <summary>
+    /// Extension of the LimbConfiguration class used to describe an arm
+    /// </summary>
+    /// <author>Antonio Terpin</author>
     public class ArmConfiguration : LimbConfiguration
     {
         public ArmConfiguration(Sensor shoulder, Sensor elbow, Sensor hand)
