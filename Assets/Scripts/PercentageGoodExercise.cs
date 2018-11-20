23568df3a208692bfccb175bd54e809415ca9956
Riccardo Giorato <giorat.96@gmail.com>
2018-11-20 11:42:00 +0100

    added percentage and gizmos

---
+++ Assets/Scripts/PercentageGoodExercise.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PercentageGoodExercise : MonoBehaviour {
+
+	public delegate void SendPercentageChanged (int percentageInt);
+	public static SendPercentageChanged EventSendPercentageExercise;
+
+	private float numberOfTakenSamples;
+
+	private float totalSumOfCorrectSamples;
+
+	private float cooldownTick;
+
+	public float tickCadence;
+
+	// Use this for initialization
+	void Start () {
+		Reset ();
+	}
+
+	public void Reset () {
+		numberOfTakenSamples = 0;
+		totalSumOfCorrectSamples = 0;
+		cooldownTick = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		cooldownTick -= Time.deltaTime;
+		if (cooldownTick <= 0)
+			SendPercentageExercise ();
+	}
+
+	void SendPercentageExercise () {
+		if (numberOfTakenSamples != 0) {
+			int percentage = Mathf.RoundToInt (totalSumOfCorrectSamples / numberOfTakenSamples * 100);
+			if (EventSendPercentageExercise != null)
+				EventSendPercentageExercise (percentage);
+		}
+	}
+
+	void ExerciseResult (string limb, bool correctPosition) {
+		numberOfTakenSamples += 1;
+		if (correctPosition)
+			totalSumOfCorrectSamples += 1;
+	}
+
+}
---
+++ Assets/TolleranceController/Readme.txt
@@ -0,0 +1,5 @@
+== TOLLERANCE AREA DIMENSION VIEWER ==
+HOW TO USE
+1 - Put TolleranceGizmoPrefab as a child of element to show tollerance area.
+2 - Set radius to set tollerance area dimension.
+3 - Use DrawTolleranceGizmo method to refresh draw of gizmo or change color.
